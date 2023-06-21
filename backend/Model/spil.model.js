551a6f27e41006d83ac35e4d0a60a7af9ff86e19
simo994q <simonkn1@hotmail.com>
2023-06-21 09:35:36 +0200

    spilmodel

--- backend/Model/spil.model.js
+++ backend/Model/spil.model.js
@@ -1,1 +1,69 @@
 // Model er den måde API'et vil blive visuelt opsat på 
+import { sequelize } from "../Config/db.sequelize.js";
+import { Sequelize, DataTypes, Model } from "sequelize";
+
+class SpilModel extends Model {}
+
+SpilModel.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    desc: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    platform_type: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    image: {
+        type: DataTypes.BLOB,
+        allowNull: false
+    },
+    version: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    language: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    release_date: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    genre: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    developer_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    players: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize,
+    modelName: 'spil',
+    freezeTableName: true,
+    underscored: true,
+})
+
+export default SpilModel
