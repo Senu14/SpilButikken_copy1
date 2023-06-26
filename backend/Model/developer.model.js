d81d2e47394c0189c4a6f54881f5bed30b21a453
simo994q <simonkn1@hotmail.com>
2023-06-26 12:18:13 +0200

    fix

--- backend/Model/developer.model.js
+++ backend/Model/developer.model.js
@@ -3,19 +3,17 @@
 import { sequelize } from "../Config/db.sequelize.js";
 import { Model, DataTypes } from "sequelize";
 
-DataTypes
-
 class developerModel extends Model { }
 
 developerModel.init({
     id: {
-        type: Datatypes.BIGINT, //ID typen skal være tal
+        type: DataTypes.BIGINT, //ID typen skal være tal
         allowNull: false, //må ikke efterlades tom
         primaryKey: true //ID er den primære nøgle og er unik. Intet kan hedde det samme
     },
 
     name: {
-        type: Datatypes.VARCHAR,
+        type: DataTypes.VARCHAR,
         allowNull: false
     }
 }), {
