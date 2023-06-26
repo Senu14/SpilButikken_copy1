d8c768c49d1ac657c04ed2a98d72294d4cd468d6
Senu14 <101559338+Senu14@users.noreply.github.com>
2023-06-26 09:01:52 +0200

    changes

--- frontend/src/Components/Partials/Footer/Footer.jsx
+++ frontend/src/Components/Partials/Footer/Footer.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
-import './Footer.scss'
-
+import React from 'react';
 
 const Footer = () => {
   return (
     <footer>
       <div>
-        <p>&copy; {new Date().getFullYear()} My Website. All rights reserved.</p>
+        <h2>Payment Methods</h2>
+        <img src="../../../Images/pngegg.png" alt="masstercard" />
+        <img src="/path/to/payment2.png" alt="Payment Method 2" />
+        <img src="/path/to/payment3.png" alt="Payment Method 3" />
+      </div>
+      <div>
+        <h2>Follow Us</h2>
+        <a href="https://www.facebook.com">
+          <img src="../../../Images/65585-program-messenger-logo-facebook-honors-icon.png" alt="Facebook" />
+        </a>
+        <a href="https://www.twitter.com">
+          <img src="/path/to/twitter.png" alt="Twitter" />
+        </a>
+        <a href="https://www.instagram.com">
+          <img src="../../../Images/69800-logo-computer-instagram-icons-free-clipart-hq.png" alt="Instagram" />
+        </a>
       </div>
     </footer>
   );
 };
 
 export default Footer;
-
-
-
-
-
-
--- frontend/src/Pages/Contact/Contact.jsx
+++ frontend/src/Pages/Contact/Contact.jsx
@@ -1,11 +1,69 @@
-import React from 'react';
-import './Contact.scss';
+import React, { useState } from 'react';
 
-const Contact= () => { 
+const Contact = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleMessageChange = (event) => {
+    setMessage(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // form submission logic here
+    // we can use an API call or any other method to send the form data
+
+    // Reset form fields
+    setName('');
+    setEmail('');
+    setMessage('');
+  };
+
   return (
-    <div>Contact</div>
-  )
-}
-
+    <form className='global-body-color' onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={handleNameChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          value={message}
+          onChange={handleMessageChange}
+          required
+        />
+      </div>
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
 
 export default Contact;
+
