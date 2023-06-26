661a1f21d0b632cb37a104fbba2dd7c7c1d3be25
simo994q <simonkn1@hotmail.com>
2023-06-26 10:47:33 +0200

    fix images

--- backend/Controllers/games.controller.js
+++ backend/Controllers/games.controller.js
@@ -3,6 +3,12 @@ import GamesModel from '../Model/games.model.js'
 class GamesController {
     list = async (req, res) => {
         const result = await GamesModel.findAll()
+        
+        result.forEach(element =>{
+            let image = new Buffer.from(element.image).toString('utf8')
+            element.image = image
+        })
+
         res.json(result)
     }
 
